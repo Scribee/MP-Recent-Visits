71bf011499500ed4f0f9ba252bd8bf3ab1fa46df
Scribee <c1sloan6@gmail.com>
2025-02-17 12:53:07 -0700

    Improve response time for routes with no ticks, and improve documentation

--- scripts/area_iframe_content.js
+++ scripts/area_iframe_content.js
@@ -1,21 +1,21 @@
+// Do nothing if not in an iframe
 if (window.self === window.top) {
-    console.log("iframe content script called out of an iframe");
+
 }
 else {
-    console.log("area_iframe_content script running in iframe", window.name);
+    //console.log("area_iframe_content running in iframe", window.name);
     const routeList = document.querySelector("table#left-nav-route-table tbody");
     // Check if the area contains routes
     if (routeList) {
         const routeLinks = routeList.querySelectorAll("td a:first-child"); // ignores remove todo links
 
-        let i = 0;
-
+        let i = 0; // suffixes are added to ensure iframe ids are unique but keep the same target tab id
+        // Load all child routes in iframes
         routeLinks.forEach(function (link) {
             let url = link.href.split("/"); // generate the url for the stats of the current route
             url.splice(4, 0, "stats");
 
-            console.log("Sending message to background to open sub-area route:", url.join("/"));
-
+            //console.log("Sending message to background to open sub-area route:", url.join("/"));
             chrome.runtime.sendMessage({
                 type: "send-area-route-to-offscreen-page",
                 target: "background",
@@ -29,9 +29,10 @@ else {
 
         // Check if the area has sub areas
         if (subAreas) {
-            let i = 0
+            let i = 0;
+            // Load all child areas in iframes
             subAreas.forEach(function(area) {
-                console.log("Opening sub area", area.href, "id", window.name + "-" + i);
+                //console.log("Opening sub area", area.href, "id", window.name + "-" + i);
                 chrome.runtime.sendMessage({
                     type: "send-area-route-to-offscreen-page",
                     target: "background",
@@ -45,7 +46,7 @@ else {
         }
     }
 
-    // Close the frame after the child pages have been opened
+    // Close the area frame after the child pages have been opened
     chrome.runtime.sendMessage({
         type: "close-frame-request",
         target: "offscreen",
--- scripts/stats_content.js
+++ scripts/stats_content.js
@@ -1,61 +1,79 @@
-let recentTicks = [];
-let sent = false;
+// Do nothing if not in an iframe
+if (window.self === window.top) {
 
-// Get initially empty stats table element
-const stats_table = document.querySelector("div#route-stats div.onx-stats-table div.col-lg-6 tbody");
-
-let observer = new MutationObserver(getDate);
-// Wait for table to be populated
-observer.observe(stats_table, {
-    childList: true,
-    subtree: true
-});
-
-// Consider making timeout adjustable for users with poor wifi
-setTimeout(sendTickCount, 10000);
-
-// If the mutation added a strong element, extract the date and add it to the recentTicks array
-function getDate(mutation) {
-    observer.disconnect();
-
-    mutation.forEach(function(record) {
-        if (sent || !record.addedNodes) {
-            return;
-        }
-
-        // Try to get the element with the tick date
-        let dateElement = record.addedNodes[0].querySelector("div.small div strong");
-
-        if (!dateElement) {
-            return;
-        }
-
-        let date = Date.parse(dateElement.textContent);
-
-        // Read ticks not in the future
-        if (Date.now() - date > 0) {
-            recentTicks.push(date); // add to array
-        }
-
-        // Once a week old tick or 10 recent ticks are found, stop parsing
-        if (Date.now() - date > 691200000 || recentTicks.length > 9) {
-            sendTickCount();
-            sent = true;
-            return;
-        }
-    });
 }
+else {
+    let recentTicks = [];
+    let sent = false;
 
-// Send the number of ticks, most recent tick date, and tab ID to the background script
-async function sendTickCount() {
-    if (sent) {
-        return false;
+    // Get initially empty stats table element
+    const stats_table = document.querySelector("div#route-stats div.onx-stats-table div.col-lg-6 div");
+
+    let observer = new MutationObserver(getDate);
+    // Wait for table to be populated
+    observer.observe(stats_table, {
+        childList: true,
+        subtree: true
+    });
+
+    // Consider making timeout adjustable for users with poor wifi
+    setTimeout(sendTickCount, 10000);
+
+    // If the mutation added a strong element, extract the date and add it to the recentTicks array
+    function getDate(mutation) {
+        console.log("mutation");
+        observer.disconnect();
+
+        mutation.forEach(function(record) {
+            if (sent || record.addedNodes.length == 0) {
+                console.log("skipping record,", sent);
+                return;
+            }
+
+            console.log("record", record.addedNodes);
+
+            // Try to get the element with the tick date
+            let dateElement = record.addedNodes[0].querySelector("div.small div strong");
+
+            if (!dateElement) {
+                // If there are no ticks for this route, send the response immediately
+                if (record.addedNodes[0].textContent === "0") {
+                    sendTickCount();
+                    sent = true;
+                    return;
+                }
+
+                return;
+            }
+
+            let date = Date.parse(dateElement.textContent);
+
+            // Read ticks not in the future
+            if (Date.now() - date > 0) {
+                recentTicks.push(date); // add to array
+            }
+
+            // Once a week old tick or 10 recent ticks are found, stop parsing
+            if (Date.now() - date > 691200000 || recentTicks.length > 9) {
+                sendTickCount();
+                sent = true;
+                return;
+            }
+        });
     }
 
-    chrome.runtime.sendMessage({
-        type: "iframe-response",
-        target: "background",
-        data: {visits: recentTicks.length - 1, lastVisit: recentTicks[recentTicks.length - 1], tab: window.name}
-    });
-    return true;
+    // Send the number of ticks, most recent tick date, and tab ID to the background script
+    async function sendTickCount() {
+        if (sent) {
+            return false;
+        }
+
+        chrome.runtime.sendMessage({
+            type: "iframe-response",
+            target: "background",
+            data: {visits: recentTicks.length - 1, lastVisit: recentTicks[recentTicks.length - 1], tab: window.name}
+        });
+
+        return true;
+    }
 }
